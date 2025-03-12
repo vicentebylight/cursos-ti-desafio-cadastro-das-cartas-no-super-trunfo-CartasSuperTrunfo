4a931ce9904ddc7b1892bb7866538deab9d39581
vicentebylight <vicentebylight@gmail.com>
2025-03-12 20:14:38 +0000

    iniciei o preenchimento

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,6 +4,16 @@ int main() {
     
     printf ("Desafio Super Trunfo - Países\n");
 
-    
+    printf ("Carta_01\n");
+
+    char estado [30] = "Ceara\n";
+    char codigo [20] = "A01\n";
+    char nome_da_cidade [30] = "Fortaleza\n";
+    int populacao = "20000000\n";
+    float area = 10000;
+    float PIB = 20000000;
+    int pontos_turisticos = 99;
+
+
     return 0;
 }
