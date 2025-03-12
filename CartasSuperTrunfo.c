b3727359394f0ac1f4c39a9d377972076886ade9
vicentebylight <vicentebylight@gmail.com>
2025-03-12 21:04:10 +0000

    preenchendo tudo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,6 +14,40 @@ int main() {
     float PIB = 20000000;
     int pontos_turisticos = 99;
 
+    printf ("Carta_02\n");
 
-    return 0;
+    char estado [30] = "Pernambuco\n";
+    char codigo [20] = "A02\n";
+    char nome_da_cidade [30] = "Recife\n";
+    int populacao = "300000000\n";
+    float area = 20000;
+    float PIB = 40000000;
+    int pontos_turisticos = 50;
+
+
+printf ("Estado: \n");
+scanf ("%s, &estado/n");
+
+printf ("Codigo da carta: \n");
+scanf ("%s, &codigo\n");
+
+printf ("Cidade: \n");
+scanf ("%s, &nome_da_cidade\n");
+
+printf ("Populacao: \n");
+scanf ("%d, &populacao: \n");
+
+printf ("Area: \n");
+scanf ("%f, &area\n");
+
+printf ("PIB: \n");
+scanf ("%f, &PIB\n");
+
+printf ("Numero de pontos turisticos: \n");
+scanf ("%d, &pontos_turisticos\n");
+
+printf ("Carta")
+printf ("Carta: %s, carta\n");
+printf ("Estado: %s, &estado\n");
+printf ("Codigo")
 }
