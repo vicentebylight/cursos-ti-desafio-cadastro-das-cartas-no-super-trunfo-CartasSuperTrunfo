57305b4976e3c055667c66933a1602ae35688900
vicentebylight <vicentebylight@gmail.com>
2025-03-13 22:22:51 +0000

    finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,27 +7,48 @@ int main() {
 
     printf ("Carta_01\n");
 
-    char estado [10] = "A";
-    char codigo [10] = "A01\n";
-    char cidade [30] = "Fortaleza\n";
     int populacao = 200.000;
-    float area = 100.10;
+    float area = 100.100;
+    float PIB = 200.000;
     int pontos = 25;
 
-    
-printf ("Estado: %s\n Codigo: %s\n Cidade: %s\n população: %d\n Area: %f\n Pontos turisticos: %d\n", estado, codigo, cidade, populacao, area, pontos);
+    printf ("População: \n");
+    scanf ("%d, &populacao");
+
+    printf ("Area: \n");
+    scanf ("%f, &area");
+
+    printf ("PIB: \n");
+    scanf ("%f, &PIB");
+
+    printf ("Pontos turisticos: \n");
+    scanf ("%d, &pontos");
+
+printf ("População: %d\n Area: %f\n PIB: %f \n Pontos turisticos: %d\n", populacao, area, PIB, pontos);
+
 
 printf ("Carta_02\n");
 
-    char estado [10] = "B";
-    char codigo [20] = "B01\n";
-    char cidade [30] = "Recife\n";
     int populacao = 300.000;
-    float area = 200.20;
+    float area = 200.200;
+    float PIB = 300.000;
     int pontos = 50;
 
-    printf ("Estado: %s\n Codigo: %s\n Cidade: %s\n população: %d\n Area: %f\n Pontos turisticos: %d\n", estado, codigo, cidade, populacao, area, pontos);
-    
+    printf ("População: \n");
+    scanf ("%d, &populacao");
+
+    printf ("Area: \n");
+    scanf ("%f, &area");
+
+    printf ("PIB: \n");
+    scanf ("%f, &PIB");
+
+    printf ("Pontos turisticos: \n");
+    scanf ("%d, &pontos");
+
+    printf ("População: %d\n Area: %f\n PIB: %f \n Pontos turisticos: %d\n", populacao, area, PIB, pontos);
+
+
 return 0;
 
 }
