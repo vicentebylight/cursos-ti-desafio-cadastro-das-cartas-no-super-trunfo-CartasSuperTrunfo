4b34917ec57b45fc910b809155da2eb27e00d5a5
vicentebylight <vicentebylight@gmail.com>
2025-03-24 15:47:11 -0300

    Atualizar o CartasSuperTrunfo.c
    Desafioaventureirofinalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,29 +9,27 @@ int main() {
     int populacao1 = 100000;
     float area1 = 100000.0;
     float PIB1 = 150000.0;
-    int pontos1 = 25 ;
+    int pontos1 = 25;
     float densidade1;
     float PIBpercapita1;
 
-    printf("População: ", populacao1);
+    printf("População: ");
     scanf("%d", &populacao1);
 
-    printf("Área: ", area1);
+    printf("Área: ");
     scanf("%f", &area1);
 
-    printf("PIB: ", PIB1);
+    printf("PIB: ");
     scanf("%f", &PIB1);
 
-    printf("Pontos turísticos: ", pontos1);
+    printf("Pontos turísticos: ");
     scanf("%d", &pontos1);
 
-    densidade1 = populacao1 / area1;
+    densidade1 = (float)populacao1 / area1;
     PIBpercapita1 = PIB1 / populacao1;
 
-
     printf("\nDados da Carta 01:\n");
-
-    printf("População: %d\nÁrea: %.2f\nPIB: %.2f\nPontos turísticos: %d\n Densidade Populacional: %.2f\n PIB per capita: %.2f\n",
+    printf("População: %d\nÁrea: %.2f\nPIB: %.2f\nPontos turísticos: %d\nDensidade Populacional: %.2f\nPIB per capita: %.2f\n",
          populacao1, area1, PIB1, pontos1, densidade1, PIBpercapita1);
 
     // Carta 02
@@ -44,24 +42,24 @@ int main() {
     float densidade2;
     float PIBpercapita2;
 
-    printf("População: ", populacao2);
+    printf("População: ");
     scanf("%d", &populacao2);
 
-    printf("Área: ", area2);
+    printf("Área: ");
     scanf("%f", &area2);
 
-    printf("PIB: ", PIB2);
+    printf("PIB: ");
     scanf("%f", &PIB2);
 
-    printf("Pontos turísticos: ", pontos2);
+    printf("Pontos turísticos: ");
     scanf("%d", &pontos2);
 
-    densidade2 = populacao2 / area2;
+    densidade2 = (float)populacao2 / area2;
     PIBpercapita2 = PIB2 / populacao2;
 
     printf("\nDados da Carta 02:\n");
-    printf("População: %d\n Área: %.2f\n PIB: %.2f\n Pontos turísticos: %d\n Densidade Populacional: %.2f\n PIB per capita: %.2f\n", 
-           populacao2, area2, PIB2, pontos2,densidade2, PIBpercapita2);
+    printf("População: %d\nÁrea: %.2f\nPIB: %.2f\nPontos turísticos: %d\nDensidade Populacional: %.2f\nPIB per capita: %.2f\n", 
+           populacao2, area2, PIB2, pontos2, densidade2, PIBpercapita2);
 
     return 0;
 }
